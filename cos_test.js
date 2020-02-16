47a307497c776c377cb358ae483d01582e4a9c6c
Lance Liang <LanceLiang2018@163.com>
2020-02-16 16:47:06 +0800

    'init'

---
+++ cos_test.js
@@ -0,0 +1,7 @@
+var Bucket = 'blog-1254016670';
+var Region = 'ap-chengdu';
+
+var cos = new COS({
+    SecretId: 'AKIDcq7HVrj0nlAWUYvPoslyMKKI2GNJ478z',
+    SecretKey: '70xZrtGAwmf6WdXGhcch3gRt7hV4SJGx',
+});
