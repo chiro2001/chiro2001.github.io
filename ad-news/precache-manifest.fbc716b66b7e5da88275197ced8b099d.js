e1523ea2dd93d1419817c0813b29362622915d06
Chiro <chiro2001@163.com>
2020-08-28 04:46:57 +0800

    add ad-news(error)

---
+++ ad-news/precache-manifest.fbc716b66b7e5da88275197ced8b099d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98b6254638bbc8ccc88494537fbdc9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d874339701714455de20",
+    "url": "/static/css/2.63cd7831.chunk.css"
+  },
+  {
+    "revision": "3a766a61bf8f356644c8",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d874339701714455de20",
+    "url": "/static/js/2.0da9da3e.chunk.js"
+  },
+  {
+    "revision": "7d62a07649f75e469697813f316b7a39",
+    "url": "/static/js/2.0da9da3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a766a61bf8f356644c8",
+    "url": "/static/js/main.0f60e717.chunk.js"
+  },
+  {
+    "revision": "f4a62f5b69daa305dbb2",
+    "url": "/static/js/runtime-main.f6dcc28c.js"
+  }
+]);
