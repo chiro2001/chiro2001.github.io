41a742bc15f57f1ecad45e67c0462959f83a78b2
Chiro <chiro2001@163.com>
2020-09-16 21:27:45 +0800

    add hitsz-sign

---
+++ sign/precache-manifest.206882249a0dd986d13a4586fafcae35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babde01d2eaabc60d318edde5aaf3d83",
+    "url": "/sign/index.html"
+  },
+  {
+    "revision": "495a2ad8cafebe628ecc",
+    "url": "/sign/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d05e0775b6c7447ec68e",
+    "url": "/sign/static/js/2.fcbb5a39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sign/static/js/2.fcbb5a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495a2ad8cafebe628ecc",
+    "url": "/sign/static/js/main.68656847.chunk.js"
+  },
+  {
+    "revision": "feff8a8da731da589bae",
+    "url": "/sign/static/js/runtime-main.f0eb8284.js"
+  }
+]);
